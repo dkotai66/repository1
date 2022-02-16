138eb65882582b55b7f200276a0f5f6a715ceaff
Kótai Dániel <dkotai66@gmail.com>
2022-02-16 13:05:51 +0100

    1. feladat kész

---
+++ SorozatKiir/SorozatKiir/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorozatKiir
+{
+    class Program
+    {
+
+        static void SorozatKiir() {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            SorozatKiir();
+        }
+    }
+}
